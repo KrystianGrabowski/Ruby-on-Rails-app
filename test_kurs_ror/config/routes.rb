62c4e84812a1c6c16379a2d484241cf3e452d61d
Unknown <krystian987543@gmail.com>
2018-10-27 18:54:02 +0200

    Active admin

--- test_kurs_ror/config/routes.rb
+++ test_kurs_ror/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+  devise_for :models
   root 'pages#home'
   get 'pages/about'
   get 'pages/home'
