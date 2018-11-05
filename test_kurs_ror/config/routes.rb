d32bf797d6720fd7959f224204c813e2dc65f471
Unknown <krystian987543@gmail.com>
2018-11-05 21:06:52 +0100

    Add Navbar

--- test_kurs_ror/config/routes.rb
+++ test_kurs_ror/config/routes.rb
@@ -10,5 +10,7 @@ Rails.application.routes.draw do
   get 'pages/home'
   get 'pages/contact'
   resources :comments
+
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
