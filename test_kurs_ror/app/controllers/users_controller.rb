6404c7b374e4beb26ad1d32e61ec2f0cf13c7a44
Adrian <adrienkaminskig@gmail.com>
2018-11-13 20:59:04 +0100

    User profile with comment list

--- test_kurs_ror/app/controllers/users_controller.rb
+++ test_kurs_ror/app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
-  before_action :authenticate_admin_user!
+  before_action :authenticate_admin_user!,  except: %i[show]
 
+  def show 
+    @user = User.find(params[:id])
+    @comments = Comment.all.find_all{|c| c.user_id == @user.id}.sort.group_by{|c| c.product}
+
+  end
   def index
     @users = User.all
   end
