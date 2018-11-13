809e677c6402a3f1e02f5fa6a8cd064b77f7ccf6
Adrian <adrienkaminskig@gmail.com>
2018-11-13 23:03:27 +0100

    Active storage

--- test_kurs_ror/app/controllers/products_controller.rb
+++ test_kurs_ror/app/controllers/products_controller.rb
@@ -1,14 +1,17 @@
 class ProductsController < InheritedResources::Base
-  before_action :authenticate_admin_user!, except: %i[index show down reservation]
+  before_action :authenticate_admin_user!, except: %i[index show show_picture down reservation]
 
   def show
     @product = Product.find(params[:id])
     @comments = Comment.new(product: @product)
+    
   end
 
   def product_params
-    params.require(:product).permit(:name, :price, :description, :category, :amount)
+    params.require(:product).permit(:name, :remove_picture, :picture, :price, :description, :category, :amount)
   end
+
+
 
   def down
     @product = Product.find(params[:id])
