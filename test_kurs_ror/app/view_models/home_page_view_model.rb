2005a400a5073bc2c8ce96d931dbd9342debc296
Patryk Kumor <patryk_kumor@wp.pl>
2018-11-19 21:49:07 +0100

    Visuals and nil fixes

--- test_kurs_ror/app/view_models/home_page_view_model.rb
+++ test_kurs_ror/app/view_models/home_page_view_model.rb
@@ -2,14 +2,16 @@ class HomePageViewModel
 
   def featured_product
     Product.last
+    
   end
 
   def new_products
     Product.where.not(id: featured_product.id).last(3)
+    
   end
 
   def pupular_products
-    Product.joins(:comments).group(:product_id, :id).order('count(product_id) desc').limit(3)
+    Product.joins(:comments).group(:product_id, :id).order('count(product_id) desc').limit(3) 
   end
 
 end
