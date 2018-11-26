358f9c609191bbb0a7cb8d9e9162a138f5d2ec80
KrystianGrabowski <krystian987543@gmail.com>
2018-11-26 23:46:40 +0100

    Booking tests

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,10 @@
 class Booking < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :product
+
+  def display_status
+    return true if returned?
+    end_date
+  end
+
 end
---
+++ spec/models/booking_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe Booking do
+  let(:product) { FactoryBot.create :product }
+  context 'when booking is created' do
+    let(:booking) {product.bookings.create!}
+
+    it 'always relates to the same product' do
+      expect(product).to eq(booking.product)
+    end
+
+    it 'has returned field set to false' do
+      expect(booking.returned).to be_falsey
+    end
+  end
+
+  describe '#display_status' do
+    let(:booking) {product.bookings.create(returned: true)}
+    it do
+      expect(booking.display_status).to be true      
+    end
+  end
+
+
+
+end
