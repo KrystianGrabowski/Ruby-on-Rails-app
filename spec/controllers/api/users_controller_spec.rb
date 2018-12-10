67af7009a0678f2c92921cd935b27d482d5bdad6
KrystianGrabowski <krystian987543@gmail.com>
2018-12-10 23:35:52 +0100

    Email validation tests

---
+++ spec/controllers/api/users_controller_spec.rb
@@ -0,0 +1,51 @@
+require 'rails_helper'
+
+RSpec.describe Api::UsersController do
+  describe 'GET #email_exists' do
+    subject { get :email_exists, params: { email: email } }
+    let(:email) { 'user@example.com' }
+
+    it do
+      expect(subject).to be_successful
+      expect(JSON.parse(subject.body)).to eq(false)
+    end
+
+    context 'when email is taken' do
+      let(:user) { create :user }
+      let(:email) { user.email }
+
+      it do
+        expect(subject).to be_successful
+        expect(JSON.parse(subject.body)).to eq(true)
+      end
+    end
+  end
+
+  describe 'GET #email_correct' do
+    subject { get :email_correct, params: { email: email } }
+    let(:email) { 'user@example.com' }
+
+    it do
+      expect(subject).to be_successful
+      expect(JSON.parse(subject.body)).to eq(true)
+    end
+
+    context 'when email is invalid1' do
+      let(:email) { 'user@example' }
+
+      it do
+        expect(subject).to be_successful
+        expect(JSON.parse(subject.body)).to eq(false)
+      end
+    end
+
+    context 'when email is invalid2' do
+      let(:email) { 'userexample.com' }
+
+      it do
+        expect(subject).to be_successful
+        expect(JSON.parse(subject.body)).to eq(false)
+      end
+    end
+  end
+end
