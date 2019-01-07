ab1f78c52b17a8328d0a0ffb977f46478867b4fe
Patryk Kumor <patryk_kumor@wp.pl>
2019-01-07 21:59:26 +0100

    Simple sort

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,13 @@
 module ApplicationHelper
-  def sorting_link(base_path, direction, current_sorting, other_params)
-    css_class = if current_sorting == direction
-                  'active'
-                else
-                  ''
-                end
-
+  def sorting_link(base_path, direction, other_params)
     url_params = { sort_by: direction }.merge(other_params)
     url = "#{base_path}?#{url_params.to_query}"
-    "<li class='#{css_class}'><a href='#{url}'>#{direction}</a></li>".html_safe
+    "<a href='#{url}'>#{direction}</a>".html_safe
+  end
+
+  def category_link(base_path, new_category)
+    url_params = { category: new_category }
+    url = "#{base_path}?#{url_params.to_query}"
+    "<a href='#{url}'>#{new_category}</a> <br>".html_safe
   end
 end
