704908aa39c7c96f9a242f02b15be00aa0b9d23c
Unknown <krystian987543@gmail.com>
2019-01-07 23:13:24 +0100

    User profile final

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :authenticate_admin_user!, except: %i[show]
+  before_action :authenticate_admin_user!, except: %i[show edit]
   add_breadcrumb 'Users', :users_path
   def show
     @user = User.find(params[:id])
@@ -10,11 +10,14 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
     add_breadcrumb @user.display_name.to_s, :user_path
+    
   end
 
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
+    redirect_to user_path
+    flash[:notice] = 'Edytowano profil!'
   end
 
   def user_params
