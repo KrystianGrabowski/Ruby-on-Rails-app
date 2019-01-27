e718db56987c7c2c21f78df4f5897c0f5adcd413
Adrian <adrienkaminskig@gmail.com>
2019-01-27 18:54:36 +0100

    Doc update
    application, carts, users controller

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,12 +1,14 @@
+# Kontroler odpowiedzialny za koszyk.
 class CartsController < ApplicationController
   before_action :authenticate_user!, only: %i[finalize]
-
+  # Dodaje okruchy i przesyła dane do widoku
   def index
     add_breadcrumb 'Cart', :carts_path
     @cart = current_guest.cart
     @value = @cart.cart_sum_check
   end
 
+  # Finalizuje zamowienie. Wyświetla notkę informacyjną oraz przenosi do produków.
   def finalize
     s = current_guest.cart.cart_check
     current_guest.cart.order_items.each do |item|
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,23 @@
+# Kontroler odpowiedzialny za użytkowników.
 class UsersController < ApplicationController
   before_action :authenticate_admin_user!, except: %i[show edit update user_params]
   before_action :correct_user, only: %i[edit update user_params]
   add_breadcrumb 'Users', :users_path
+
+  # Znajduje użytkownika po numerze identyfikacyjnym, dodaje okruchy oraz wyświetla jego komentarze.
   def show
     @user = User.find(params[:id])
     add_breadcrumb @user.display_name.to_s, :user_path
     @comments = @user.comments.sort.group_by(&:product)
   end
 
+  # Znajduje użytkownika po numerze identyfikacyjnym, dodaje okruchy oraz przenosi do strony umożliwiającej obróbkę danych użytkownika.
   def edit
     @user = User.find(params[:id])
     add_breadcrumb @user.display_name.to_s, :user_path
   end
 
+  # Znajduje użytkownika po numerze identyfikacyjnym, przenosi do strony użytkownika oraz wyświetla notkę informacyjną.
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
@@ -20,14 +25,17 @@ class UsersController < ApplicationController
     flash[:notice] = 'Edytowano profil!'
   end
 
+  # @return zwraca parametry użytkownika.
   def user_params
     params.require(:user).permit!
   end
 
+  # @return [User] Zwraca wszystkich użytkowników.
   def index
     @users = User.all
   end
 
+  # Znajduje użytkownika po numerze identyfikacyjnym, usuwa go, przenosi do listy wszystkich użytkowników oraz wyświetla notkę informacyjną.
   def destroy
     @user = User.find(params[:id])
     @user.destroy
@@ -39,6 +47,9 @@ class UsersController < ApplicationController
 
   private
 
+  # Sprawdza czy odpowiedni użytkownik jest zalogowany.
+  # @return [true] jeśli tak
+  # @return [false] jeśli nie
   def correct_user
     if user_signed_in?
       return true if current_user.id.to_s == params[:id]
